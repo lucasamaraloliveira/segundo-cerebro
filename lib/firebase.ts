ee389b3f9c5f4fdf603070c2a833b0079b487c20
Lucas Amaral Oliveira da Silva <37918350+lucasamaraloliveira@users.noreply.github.com>
2026-04-22 16:18:36 -0300

    fix: ajustado erro de compilacao vercel
    fix: ajustado erro de compilacao vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ import firebaseConfig from '../firebase-applet-config.json';
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 
 // Enable offline persistence
